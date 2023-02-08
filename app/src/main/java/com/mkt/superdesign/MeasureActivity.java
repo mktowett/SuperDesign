e454ae7ccafd6888e0afc9a432d941e54f9c81ed
mktowett <marvintowett@gmail.com>
2023-02-08 11:13:38 +0300

    Initial commit

---
+++ app/src/main/java/com/mkt/superdesign/MeasureActivity.java
@@ -0,0 +1,13 @@
+package com.mkt.superdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeasureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measure);
+    }
+}
